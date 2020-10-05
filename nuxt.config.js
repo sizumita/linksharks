a96d24c74aa76120f8888954b21379fc210ad5e6
Sumito Izumita <sumito@izumita.com>
2020-10-05 19:03:41 +0900

    seoの変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - linksharks',
-    title: 'linksharks',
+    titleTemplate: '%s - Connect to Discord',
+    title: 'LinkSharks',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
